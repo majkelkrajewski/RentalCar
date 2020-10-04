a59479b068d917269b3885d189b41dc0f8401147
Uzytkownik <uzytkownik@kursgit.pl>
2020-10-04 12:45:30 +0200

    initial commit

---
+++ src/main/java/com/sda/RentalCar/domain/User.java
@@ -0,0 +1,22 @@
+package com.sda.RentalCar.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@Data
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id
+}
